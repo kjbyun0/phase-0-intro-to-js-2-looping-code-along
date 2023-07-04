15455e8bcafaac01f1ef51d6d4a106a0b92bbea2
Ki Jong Byun <kjbyun0@gmail.com>
2023-07-03 23:59:39 -0500

    Phase1 Assignment Done

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-function writeCards(names, eventName) {
-    const cards = [];
-    for (let i = 0; i < names.length; i++) {
-        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
-    }
-    return cards;
-}
 
-//const newCards = writeCards(["Charlie", "Samip", "Ali"], "birthday");
-//console.log(newCards);
+function writeCards(names, event) {
+    const messages = [];
+    for (const name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
 
 function countDown(count) {
     while (count >= 0) {
-        console.log(count);
-        count--;
+        console.log(count--);
     }
 }
-
-//countDown(10);
